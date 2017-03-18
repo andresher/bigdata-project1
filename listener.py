f29eacfdcdff2fcb0e2a8e12d82e6ce878c863f1
Andres Hernandez <andres.hernandez2@upr.edu>
2017-03-18 16:11:19 -0400

    Add python tweet listener for test data

---
+++ listener.py
@@ -0,0 +1,27 @@
+import twitterconfig as cfg
+import tweepy
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+
+auth = OAuthHandler(cfg.consumer_key, cfg.consumer_secret)
+auth.set_access_token(cfg.access_token, cfg.access_secret)
+
+api = tweepy.API(auth)
+
+class MyListener(StreamListener):
+    def on_data(self, data):
+        try:
+            with open('python.json', 'a') as f:
+                f.write(data)
+                return True
+        except BaseException as e:
+            print('Error on_data: % s' % str(e))
+            return True
+
+        def on_error(self, status):
+            print(status)
+            return True
+
+twitter_stream = Stream(auth, MyListener())
+twitter_stream.filter(track=['Trump'])

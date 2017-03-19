bbc2286ca9db783d5b725db9f6df5f66e9ae512e
Andres Hernandez <andres.hernandez2@upr.edu>
2017-03-18 22:03:35 -0400

    Add Word Frequency MR Job

---
+++ wordfrequency/src/main/java/edu/uprm/cse/bigdata/wordfrequency/TwitterWFDriver.java
@@ -0,0 +1,33 @@
+package edu.uprm.cse.bigdata.keywords;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class TwitterWFDriver {
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("Usage: TwitterWFDriver <input path> <output path>");
+            System.exit(-1);
+        }
+        Job job = new Job();
+        job.setJarByClass(edu.uprm.cse.bigdata.keywords.TwitterWFDriver.class);
+        job.setJobName("Count Word Frequency");
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.setMapperClass(edu.uprm.cse.bigdata.keywords.TwitterWFMapper.class);
+        job.setReducerClass(edu.uprm.cse.bigdata.keywords.TwitterWFReducer.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+
+}
---
+++ wordfrequency/src/main/java/edu/uprm/cse/bigdata/wordfrequency/TwitterWFMapper.java
@@ -0,0 +1,40 @@
+package edu.uprm.cse.bigdata.keywords;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import twitter4j.Status;
+import twitter4j.TwitterException;
+import twitter4j.TwitterObjectFactory;
+import java.util.Arrays;
+import java.util.List;
+
+import java.io.IOException;
+
+public class TwitterWFMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    @Override
+
+    private List<String> stopWords = Arrays.asList("A", "AN", "AND", "ARE", "AS", "AT", "BE", "BY", "FOR", "FROM",
+            "HAS", "HE", "IN", "IS", "IT", "ITS", "OF", "ON", "THAT", "THE", "TO", "WAS", "WERE", "WILL", "WITH");
+
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String rawTweet = value.toString();
+
+        try {
+            Status status = TwitterObjectFactory.createStatus(rawTweet);
+            String tweet = status.getText().toUpperCase();
+            String[] tweetWordsArray = tweet.split(" ");
+            List<String> tweetWords = Arrays.asList(items);
+            for(String word in tweetWords){
+                if (!stopWords.contains(word)){
+                    context.write(new Text(word), new IntWritable(1));
+                }
+            }
+        }
+        catch(TwitterException e){
+
+        }
+
+    }
+}
